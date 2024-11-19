2381222b462fb42e0d518f546d193e1604f58b49
Yaba Ernesto <yabaernesto@gmail.com>
2024-11-20 00:39:15 +0100

    ➕ Feat: list transitions
    list transitions

--- README.md
+++ README.md
@@ -2,14 +2,14 @@
 
 # Requisitos Funcionais
 
-- [ ] O usuário deve poder criar uma nova transação
-- [ ] O usuário deve poder obter um resumo da sua conta
-- [ ] O usuário deve poder listar todas transação que ja ocorreram
-- [ ] O usuário deve poder visualizar uma transação única
+- [x] O usuário deve poder criar uma nova transação
+- [x] O usuário deve poder obter um resumo da sua conta
+- [x] O usuário deve poder listar todas transação que ja ocorreram
+- [x] O usuário deve poder visualizar uma transação única
 
 # Regras de Negócio
 
-- [ ] A transação pode ser do tipo crédito que somará ao valor total, ou débito subtrairá
+- [x] A transação pode ser do tipo crédito que somará ao valor total, ou débito subtrairá
 - [ ] Deve ser possível identificarmos o usuário entre os requisitos
 - [ ] O usuário só pode visualizar transações o qual ele criou
 
--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,32 @@ import { randomUUID } from 'node:crypto'
 import knex from 'knex'
 
 export async function transactions(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select('*')
+
+    return { transactions }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionParamsSchema.parse(request.params)
+
+    const transaction = await knex('transactions').where('id', id).first()
+
+    return { transaction }
+  })
+
+  app.get('/summary', async () => {
+    const summary = await knex('transactions')
+      .sum('amount', { as: 'amount' })
+      .first()
+
+    return { summary }
+  })
+
   app.post('/', async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
@@ -15,12 +41,16 @@ export async function transactions(app: FastifyInstance) {
       request.body,
     )
 
-    await knex('transactions').insert({
-      id: randomUUID(),
-      title,
-      amount: type === 'credit' ? amount : amount * -1,
-    })
+    try {
+      await knex('transactions').insert({
+        id: randomUUID(),
+        title,
+        amount: type === 'credit' ? amount : amount * -1,
+      })
 
-    return reply.status(201).send()
+      return reply.status(201).send()
+    } catch {
+      return reply.status(500).send({ error: 'Internal Server Error' })
+    }
   })
 }
