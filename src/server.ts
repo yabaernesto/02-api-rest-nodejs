522405b730460a9fa296f248f83dc9ed66103f87
Yaba Ernesto <yabaernesto@gmail.com>
2024-11-19 01:43:10 +0100

    inital commit

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import fastify from 'fastify'
+import { randomUUID } from 'node:crypto'
+import { knex } from './database'
+
+const app = fastify()
+
+app.get('/hello', async () => {
+  const transactions = await knex('transactions').select('*')
+
+  return transactions
+})
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('Servidor rodando!')
+  })
