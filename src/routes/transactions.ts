2a0dd73155fa60ca7c693dd97ca12c5cdc4e181f
Yaba Ernesto <yabaernesto@gmail.com>
2024-11-21 11:53:20 +0100

    feat: added vitest and supertest

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -90,6 +90,7 @@ export async function transactions(app: FastifyInstance) {
         id: randomUUID(),
         title,
         amount: type === 'credit' ? amount : amount * -1,
+        session_id: sessionId,
       })
 
       return reply.status(201).send()
